1a855e1f8238f1e63bc1ac08536f0395c79ff25d
Éverton-Victor <evertonvictorpro@gmail.com>
2022-02-21 22:10:00 -0300

    Criando página de login

--- routes.js
+++ routes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const routes = express.Router();
 const homeController = require("./src/controllers/homeController");
+const loginController = require("./src/controllers/loginController");
 
+// rota da home
 routes.get("/", homeController.index);
+
+routes.get("/login", loginController.index);
 
 module.exports = routes;
