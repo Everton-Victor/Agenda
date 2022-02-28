a0a3e200c6246eb84c9ef99d02172e892b84ab2b
Éverton-Victor <evertonvictorpro@gmail.com>
2022-02-28 00:14:18 -0300

    Cadastrando Contato

--- routes.js
+++ routes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const routes = express.Router();
 const homeController = require("./src/controllers/homeController");
 const loginController = require("./src/controllers/loginController");
+const contatoController = require("./src/controllers/contatoController");
+
+const { loginRequired } = require("./src/middlewares/middleware");
 
 // rota da home
 routes.get("/", homeController.index);
@@ -17,5 +20,14 @@ routes.post("/login/login", loginController.login);
 
 // Rota Nav  (Sair)
 routes.get("/login/logout", loginController.logout);
+
+// Rota Nav  (Contato)
+routes.get("/contato/index", loginRequired, contatoController.index);
+
+// Rota Nav  (Contato - Registrar Contatos)
+routes.post("/contato/register", loginRequired, contatoController.register);
+
+// Rota Nav  (Contato - Obter Contatos)
+routes.post("/contato/index/:id", loginRequired, contatoController.editIndex);
 
 module.exports = routes;
